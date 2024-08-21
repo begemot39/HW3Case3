ed4b9ab566e228f136031705a0cf39a38ca3f227
Vitaliy K. <begemot39@mail.ru>
2024-08-21 13:56:30 +0300

    Edits in the code format

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,10 +1,10 @@
 public class CreditPaymentService {
-    public double calculate( int loanTerm, double interestRate, int creditAmount) {
-        double monthlyInterestRate = interestRate / ( 100 * 12 );
+    public double calculate(int loanTerm, double interestRate, int creditAmount) {
+        double monthlyInterestRate = interestRate / (100 * 12);
         double scale = Math.pow(10, 6);
 
-    double ratio = ( monthlyInterestRate * ( Math.pow( monthlyInterestRate + 1, loanTerm ) ) ) /( Math.pow( monthlyInterestRate + 1, loanTerm ) - 1 );
-        ratio = ( Math.round( ratio * scale ) / scale );
+        double ratio = (monthlyInterestRate * (Math.pow(monthlyInterestRate + 1, loanTerm))) / (Math.pow(monthlyInterestRate + 1, loanTerm) - 1);
+        ratio = (Math.round(ratio * scale) / scale);
         int monthlyPayment = (int) (creditAmount * ratio);
         return monthlyPayment;
     }
